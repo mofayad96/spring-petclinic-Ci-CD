d6a66e5747e1bb581e60a635d46b9aa809f42e27
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-10-01 21:30:39 +0300

    cd

---
+++ terraform/terraform.tf
@@ -0,0 +1,10 @@
+# required provider(aws) , version of terraform
+terraform {
+required_providers {
+    aws = {
+        source  = "hashicorp/aws"
+        version = "~> 6.0"
+    }
+}
+required_version = ">= 1.2"
+}
