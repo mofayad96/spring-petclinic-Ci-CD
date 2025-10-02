1917a33a29196043aab6bd61091be4140c82f028
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2025-10-02 19:54:28 +0300

    terraform latest

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -6,5 +6,11 @@ required_providers {
         version = "~> 6.0"
     }
 }
+backend "s3" {
+    bucket         = "petclinic-deploy-613307cd"
+    key            = "terraform/state.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+}
 required_version = ">= 1.2"
 }
