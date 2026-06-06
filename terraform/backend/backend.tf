fc5c00e842a86c7d056800a05c697c708b5cc28f
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-06-06 15:49:47 +0300

    updates in ci

--- terraform/backend/backend.tf
+++ terraform/backend/backend.tf
@@ -1,9 +1,9 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "petclinic-terraform-state-bucket-12345"
-#     key            = "dev/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "petclinic-tf-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "petclinic-terraform-state-bucket-12345"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "petclinic-tf-locks"
+    encrypt        = true
+  }
+}
