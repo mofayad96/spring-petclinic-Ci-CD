10e9ae27a09ca705125e3f0ae7e4c81126e01b55
mofayad96 <cds.mohamedsalah30408@alexu.edu.eg>
2026-07-01 01:37:38 +0300

    add aws-ebs-csi-driver addon and attach required IAM policy to EKS nodes

--- terraform/eks/main.tf
+++ terraform/eks/main.tf
@@ -27,3 +27,7 @@ resource "aws_eks_node_group" "nodes" {
   instance_types = var.node_instance_types
 }
 
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = aws_eks_cluster.cluster.name
+  addon_name   = "aws-ebs-csi-driver"
+}
